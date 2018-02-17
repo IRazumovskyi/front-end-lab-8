cd03df738daf80a49fe35e7b34cd5a83948741e8
Ty4kaxd <ty4ka_xdddd@rambler.ru>
2018-02-17 22:07:37 +0200

    some fixes

--- homework_08/js/getMin.js
+++ homework_08/js/getMin.js
@@ -1,8 +1,9 @@
-function getMin(...arrays){
+function getMin(...arr){
   let min = arguments[0];
-  for(let i = 0; i< arrays.length; i++){
-    if( arrays[i] < min){
-      min = arrays[i];
+  for(let i = 0; i< arguments.length; i++){
+    console.log(arguments[i]);
+    if( arguments[i] < min){
+      min = arguments[i];
     }
   }
   return min;
