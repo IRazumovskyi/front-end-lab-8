44f00a968100634a01a77939dff677dfc0d74ce7
Ty4kaxd <ty4ka_xdddd@rambler.ru>
2018-02-25 12:10:58 +0200

    finished 10 homework

---
+++ homework_10/task1.js
@@ -0,0 +1,30 @@
+function debounce(callback, delay) {
+  let timeout = 0 ;
+  return function () {
+    timeout=clearTimeout(timeout); 
+    timeout = setTimeout(callback, delay);
+  }
+}
+let iterator = 0;
+
+function increaseIteratorBy1() {
+  iterator++;
+
+  printIteratorValue();
+}
+
+function printIteratorValue() {
+  console.log('Iterator value ', iterator);
+}
+
+var increaseIterator = debounce(increaseIteratorBy1, 1000);
+
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator(); // Should print 'Iterator value 2'
